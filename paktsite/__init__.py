3968b3591b89843c787655cc57ab13eb7a3364e5
thewarsawpakt <smallperson07@gmail.com>
2020-12-24 01:06:28 -0600

    Going insane due to a 404 (for no reason)

--- paktsite/__init__.py
+++ paktsite/__init__.py
@@ -3,3 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 import paktsite.routes
+
+app.config["UPLOAD_FOLDER"] = "paktsite/static/client/uploads/"
--- paktsite/routes.py
+++ paktsite/routes.py
@@ -1,14 +1,35 @@
-from flask import render_template, abort, send_from_directory, request
+from flask import render_template, abort, send_from_directory, request, url_for
+from werkzeug.utils import redirect, secure_filename
 
 from paktsite import app
 
+import os
+
+DISALLOWED_FILE_EXTENSIONS = {
+    "py",
+    "html",
+    "js",
+    "sh",
+    "bash"
+}
+
 @app.route("/")
 def root():
-    return render_template("public/index.html", ip=request.remote_addr)
+    return render_template("index.html", ip=request.remote_addr)
 
-@app.route("/upload")
+@app.route("/upload", methods=["GET", "POST"])
 def upload():
-    if request.remote_addr == '47.183.192.96':
-        return render_template("public/upload.html")
-    else:
-        abort(403)
+    if request.method == "GET":
+        return render_template("upload.html")
+    elif request.method == "POST":
+        file = request.files['file']
+        if file.filename == "":
+            abort(400)
+        if file.filename.split(".")[1] in DISALLOWED_FILE_EXTENSIONS:
+            abort(400)
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], secure_filename(file.filename)))
+        return redirect(url_for("upload"))
+
+@app.route("/uploads/<filename>")
+def download(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename=secure_filename(filename))
